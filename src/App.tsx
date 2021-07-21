ad9b78fb8a098b00e8fab116c87b58559c6f2757
Josh Smith <joshuadesmith@utexas.edu>
2021-07-20 20:08:14 -0500

    added basic chat dir just for giggles

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
+import ChatContainer from './chat/ChatContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Dude that is so pog.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ChatContainer></ChatContainer>
       </header>
     </div>
   );
---
+++ src/chat/ChatContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+function ChatContainer() {
+    return (
+        <div>
+            <Button variant="contained">OMEGA CHAT</Button>
+        </div>
+    );
+}
+
+export default ChatContainer;
